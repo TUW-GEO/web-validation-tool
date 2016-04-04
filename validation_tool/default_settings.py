03d98253df41ff339dc2ff3e33102af33585e51b
Christoph Paulik <christoph.paulik@geo.tuwien.ac.at>
2016-04-04 19:00:53 +0200

    Add configuration of validation datasets and prototype for OpenDAP

--- validation_tool/default_settings.py
+++ validation_tool/default_settings.py
@@ -1,13 +1,26 @@
 import os
+from validation_tool.server.datasets import XarrayDs
+
+# PATH to the downloaded ISMN data
 ISMN_PATH = os.path.join(os.path.dirname(__file__), '..', 'tests', 'test_ismn')
+
+
+# lookup between validation dataset types and the classes that are used for
+# reading, all the classes here must accept, name, fid and variables in their
+# __init__
+VALIDATION_LOOKUP = {'xray': XarrayDs}
+
 
 """list of validation datasets, where each validation dataset is defined by a
 dictionary. Each dictionary must have the following keys:
 
-* type: one of ['xray']
+* type: one of ['xray'], has to be in the validation_lookup dictionary
 * name: string of the name of the dataset
 * fid: file identifier, string of Filename or URL(for OpenDAP datasets)
 * variables: list of strings specifying the variables to read from the dataset.
 
 """
-VALIDATION_DS = []
+VALIDATION_DS = [{'type': 'xray',
+                  'name': 'cci',
+                  'fid': "http://www.geo.tuwien.ac.at:8080/thredds/dodsC/testAll/ESACCI-SOILMOISTURE-L3S-SSMV-COMBINED-197811-201512_time-13575_lat-10_lon-10.nc",
+                  'variables': ['sm']}]
