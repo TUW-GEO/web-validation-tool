038d1e3388853ba362d03209d2fc11c26bca372a
Christoph Paulik <christoph.paulik@geo.tuwien.ac.at>
2016-04-06 16:52:18 +0200

    No functional changes - Javascript formatting and linting.

--- validation_tool/static/js/ISMNMarker_class.js
+++ validation_tool/static/js/ISMNMarker_class.js
@@ -1,6 +1,4 @@
-
-
-var infoWindow=new google.maps.InfoWindow();;   //only 1 infoWindowObject so that only 1 is shown on the map simultaniously
+var infoWindow = new google.maps.InfoWindow(); //only 1 infoWindowObject so that only 1 is shown on the map simultaniously
 
 /** Responsible for markers and their appearence
 	@author Christoph Paulik - christoph.paulik@geo.tuwien.ac.at
@@ -11,39 +9,40 @@ var infoWindow=new google.maps.InfoWindow();;   //only 1 infoWindowObject so tha
 	@param {boolean} visible - if the marker should be visible from the start
 	@param {string} title - title of the marker which is shown on mouseover
 */
-function ISMNMarker(map,position,color,visible,title){
-	
-	this.iconURL="http://chart.googleapis.com/chart?chst=d_map_spin&chld=0.35|1|"+color+"|000000";
-	  
-	this.map=map;
-	
-	this.color=color;	//color it should have if activated
-	
-	this.currentColor=color;	//currentColor is the color the marker currently has could be grey if it is deactivated
-	
-	this.visible=visible;
-	
-	this.hasInfoWindow=false;
-	
-	this.infoWindow=undefined;
-	
-	this.deactivated=false;
-	
-	if(this.visible){
-		var onMap=this.map; 
-	}
-	else{
-		var onMap=null;
-	}
-	
-	this.title=title;
-	
-	this.marker=new google.maps.Marker({
-            position:position,
-            map:onMap,
-            icon:this.iconURL,
-			title:this.title
-        });
+function ISMNMarker(map, position, color, visible, title) {
+
+    this.iconURL = "http://chart.googleapis.com/chart?chst=d_map_spin&chld=0.35|1|" + color + "|000000";
+
+    this.map = map;
+
+    this.color = color; //color it should have if activated
+
+    this.currentColor = color; //currentColor is the color the marker currently has could be grey if it is deactivated
+
+    this.visible = visible;
+
+    this.hasInfoWindow = false;
+
+    this.infoWindow = undefined;
+
+    this.deactivated = false;
+
+    var onMap = null;
+
+    if (this.visible) {
+        onMap = this.map;
+    } else {
+        onMap = null;
+    }
+
+    this.title = title;
+
+    this.marker = new google.maps.Marker({
+        position: position,
+        map: onMap,
+        icon: this.iconURL,
+        title: this.title
+    });
 }
 
 /** sets the info window of the marker and adds eventlisteners. Unfortunately we can only bind events to DOM elements that already exist
@@ -51,110 +50,109 @@ function ISMNMarker(map,position,color,visible,title){
 	@param {string} html - html content of the info window
 	@param {object} parent - parent of the marker whose data viewer has to be opended if the button in the info window is clicked. should only be given if it is a station.
 */
-ISMNMarker.prototype.setInfoWindow=function(html,parent){
+ISMNMarker.prototype.setInfoWindow = function(html, parent) {
 
-	var _self=this;
-	
-	this.clickListener=google.maps.event.addListener(this.marker, 'click', function() {
-		infoWindow.setContent(html);
-		infoWindow.open(_self.map,_self.marker);
-		_self.domReadyListener=google.maps.event.addListener(infoWindow,'domready',function(){
-	
-			if(parent!=undefined){		//if infomaker is on a station rather than a network
-				if($("#compareData").length!=0){
-					$("#compareData").click(function() {
-						parent.openDataComparison();
-					});
-				}
-        if($("#viewData").length!=0){
-            $("#viewData").click(function() {
-                parent.openDataViewer();
-            });
+    var _self = this;
+
+    this.clickListener = google.maps.event.addListener(this.marker, 'click', function() {
+        infoWindow.setContent(html);
+        infoWindow.open(_self.map, _self.marker);
+        _self.domReadyListener = google.maps.event.addListener(infoWindow, 'domready', function() {
+
+            if (parent !== undefined) { //if infomaker is on a station rather than a network
+                if ($("#compareData").length !== 0) {
+                    $("#compareData").click(function() {
+                        parent.openDataComparison();
+                    });
+                }
+
+                if ($("#viewData").length !== 0) {
+                    $("#viewData").click(function() {
+                        parent.openDataViewer();
+                    });
+                }
+            }
+        });
+
+
+        google.maps.event.addListener(infoWindow, 'position_changed', function() {
+            if (parent !== undefined) { //if infomaker is on a station rather than a network
+                google.maps.event.removeListener(_self.domReadyListener);
+            }
+
+        });
+        google.maps.event.addListener(infoWindow, 'content_changed', function() {
+            if (parent !== undefined) { //if infomaker is on a station rather than a network
+                google.maps.event.removeListener(_self.domReadyListener);
+            }
+
+        });
+
+    });
+    this.closeListener = google.maps.event.addListener(infoWindow, 'closeclick', function() {
+
+        if (parent !== undefined) { //if infomaker is on a station rather than a network
+            google.maps.event.removeListener(_self.domReadyListener);
         }
-			}
-		});
-		
-		
-		google.maps.event.addListener(infoWindow,'position_changed',function(){
-			if(parent!=undefined){		//if infomaker is on a station rather than a network
-				google.maps.event.removeListener(_self.domReadyListener);	
-			}	
-	
-		});
-		google.maps.event.addListener(infoWindow,'content_changed',function(){
-			if(parent!=undefined){		//if infomaker is on a station rather than a network
-				google.maps.event.removeListener(_self.domReadyListener);	
-			}	
-	
-		});
-			
-	});
-	this.closeListener=google.maps.event.addListener(infoWindow,'closeclick',function(){
-	
-			if(parent!=undefined){		//if infomaker is on a station rather than a network
-				google.maps.event.removeListener(_self.domReadyListener);	
-			}	
-	});
-	
-	
-	this.hasInfoWindow=true;
+    });
 
-}
+
+    this.hasInfoWindow = true;
+
+};
 
 /** set marker icon with respect to zoom level
 	currently not used
 	@param {int} zoom - current zoom level
 */
-ISMNMarker.prototype.setIconWithRespectToZoom=function(zoom){
+ISMNMarker.prototype.setIconWithRespectToZoom = function(zoom) {
 
-	var scale=zoom/11+0.1;
-	if(zoom > 12){	this.iconURL="http://chart.googleapis.com/chart?chst=d_map_spin&chld="+scale+"|1|"+this.currentColor+"|9|_|"+this.title;
-	}
-	else{
-		this.iconURL="http://chart.googleapis.com/chart?chst=d_map_spin&chld="+scale+"|1|"+this.currentColor+"|000000";
-	}
-	this.marker.setIcon(this.iconURL);
+    var scale = zoom / 11 + 0.1;
+    if (zoom > 12) {
+        this.iconURL = "http://chart.googleapis.com/chart?chst=d_map_spin&chld=" + scale + "|1|" + this.currentColor + "|9|_|" + this.title;
+    } else {
+        this.iconURL = "http://chart.googleapis.com/chart?chst=d_map_spin&chld=" + scale + "|1|" + this.currentColor + "|000000";
+    }
+    this.marker.setIcon(this.iconURL);
 
-}
+};
 
 /** set marker icon to the title of the marker
 	currently not used
 */
-ISMNMarker.prototype.addTitleAsIcon=function(){
+ISMNMarker.prototype.addTitleAsIcon = function() {
 
-	this.iconURL="http://chart.googleapis.com/chart?chst=d_text_outline&chld=666666|9|h|"+this.currentColor+"|b|"+this.title;
-	this.marker.setIcon(this.iconURL);
+    this.iconURL = "http://chart.googleapis.com/chart?chst=d_text_outline&chld=666666|9|h|" + this.currentColor + "|b|" + this.title;
+    this.marker.setIcon(this.iconURL);
 
-}
+};
 
 /** set marker icon to grey icon
-*/
-ISMNMarker.prototype.deactivate=function(){
-	this.deactivated=true;
-	this.currentColor='666666';
-	this.marker.setIcon("http://chart.googleapis.com/chart?chst=d_map_spin&chld=0.35|1|"+this.currentColor+"|000000");
-	//this.hasInfoWindow=false
-}
+ */
+ISMNMarker.prototype.deactivate = function() {
+    this.deactivated = true;
+    this.currentColor = '666666';
+    this.marker.setIcon("http://chart.googleapis.com/chart?chst=d_map_spin&chld=0.35|1|" + this.currentColor + "|000000");
+    //this.hasInfoWindow=false
+};
 
 /** set marker icon to original color icon
-*/
-ISMNMarker.prototype.activate=function(){
-	this.deactivated=false;
-	this.currentColor=this.color;
-	this.marker.setIcon(this.iconURL);
-	//this.hasInfoWindow=false
-}
-/** show marker on map
-*/
-ISMNMarker.prototype.show=function(){
-    this.visible=true;
-	this.marker.setMap(this.map);
-}
-/** hide marker from map
-*/
-ISMNMarker.prototype.hide=function(){
-    this.visible=false;
-	this.marker.setMap(null);
-}
-
-
+ */
+ISMNMarker.prototype.activate = function() {
+        this.deactivated = false;
+        this.currentColor = this.color;
+        this.marker.setIcon(this.iconURL);
+        //this.hasInfoWindow=false
+};
+    /** show marker on map
+     */
+ISMNMarker.prototype.show = function() {
+        this.visible = true;
+        this.marker.setMap(this.map);
+};
+    /** hide marker from map
+     */
+ISMNMarker.prototype.hide = function() {
+    this.visible = false;
+    this.marker.setMap(null);
+};
--- validation_tool/static/js/Station_class.js
+++ validation_tool/static/js/Station_class.js
@@ -9,201 +9,200 @@
 	@param {boolean} visible - if the network should be visible from the start
 	@param {boolean} infoWindowEnabled - if the markers should have infoWindows, only necessary for static overviews
 */
-function Station(map,Network,StationObject,visible,infoWindowEnabled){
-	
-	this.map = map;
-	
-	this.location=new google.maps.LatLng(StationObject.lat,StationObject.lng);
-	
-	this.stationObject=StationObject;
-	
-	this.name=StationObject.station_name;
-	this.abbr=StationObject.station_abbr;
-	this.id=StationObject.stationID;
-	
-	this.startDate=new Date(StationObject.minimum);
-	this.endDate=new Date(StationObject.maximum);
-	
-	this.marker=new ISMNMarker(map,this.location,Network.color,visible,Network.name+"-"+this.name);	
-	
-	this.dataviewer=new ISMNDataViewer(this.id+'_dv');
-	
-	this.validationViewer=null;
-	
-	this.network=Network;
-	
-	this.visible=visible;
-	
-	this.color=Network.color;
-	
-	this.deactivated=false;
-	
-	this.infoWindowEnabled=infoWindowEnabled;
-	
-	this.validationButton=false;
-	 		 		
-	
+function Station(map, Network, StationObject, visible, infoWindowEnabled) {
+
+    this.map = map;
+
+    this.location = new google.maps.LatLng(StationObject.lat, StationObject.lng);
+
+    this.stationObject = StationObject;
+
+    this.name = StationObject.station_name;
+    this.abbr = StationObject.station_abbr;
+    this.id = StationObject.stationID;
+
+    this.startDate = new Date(StationObject.minimum);
+    this.endDate = new Date(StationObject.maximum);
+
+    this.marker = new ISMNMarker(map, this.location, Network.color, visible, Network.name + "-" + this.name);
+
+    this.dataviewer = new ISMNDataViewer(this.id + '_dv');
+
+    this.validationViewer = null;
+
+    this.network = Network;
+
+    this.visible = visible;
+
+    this.color = Network.color;
+
+    this.deactivated = false;
+
+    this.infoWindowEnabled = infoWindowEnabled;
+
+    this.validationButton = false;
+
+
 }
 /** initializes the station
 	adds info window if enabled
 */
-Station.prototype.init= function(){
+Station.prototype.init = function() {
 
-	if(this.infoWindowEnabled)this.generateInfoWindow();
+    if (this.infoWindowEnabled) this.generateInfoWindow();
 
-}
+};
 
 /** deactivates the station
-*/
-Station.prototype.deactivate=function(){
+ */
+Station.prototype.deactivate = function() {
 
-	this.deactivated=true;
-	this.marker.deactivate();
+        this.deactivated = true;
+        this.marker.deactivate();
 
-}
-/** activates the station
-*/
-Station.prototype.activate=function(){
+};
+    /** activates the station
+     */
+Station.prototype.activate = function() {
 
-	this.deactivated=false;
-	this.marker.activate();
+    this.deactivated = false;
+    this.marker.activate();
 
-}
+};
 
 /** populates info window with information from StationObject
-*/
-Station.prototype.generateInfoWindow= function(){
+ */
+Station.prototype.generateInfoWindow = function() {
 
-	var html="<div class='iwindow'>\
-			<h2>Station:</h2> "+this.name+" ("+this.stationObject.station_abbr+")<br>\
-			<h2>Network:</h2> "+this.network.name + '<br>';
-	if(this.network.networkObject.network_url!=null){
-		html+='<h2>Network URL:</h2>';
-		network_urls = this.network.networkObject.network_url.split(';');
-		$.each(network_urls, function(index,value){
-		
-			html+='<a target="_blank" href="'+value+'">'+value+'</a>'+' <br>';
-			
-			
-		});
-	
-	}	
-	if(this.stationObject.extMetadata!=null){
-	html+='<h2>Metadata URL:</h2> <a target="_blank"  href="'+this.stationObject.extMetadata+'">'+this.stationObject.extMetadata+'</a>'+'<br>';
-	}	
-	if(this.stationObject.comment!=null){
-	html+='<h2>Comment:</h2> '+this.stationObject.comment + '<br>';
-	}		
-			
-	html+='<br>\
+        var html = "<div class='iwindow'>\
+			<h2>Station:</h2> " + this.name + " (" + this.stationObject.station_abbr + ")<br>\
+			<h2>Network:</h2> " + this.network.name + '<br>';
+        if (this.network.networkObject.network_url !== null) {
+            html += '<h2>Network URL:</h2>';
+            network_urls = this.network.networkObject.network_url.split(';');
+            $.each(network_urls, function(index, value) {
+
+                html += '<a target="_blank" href="' + value + '">' + value + '</a>' + ' <br>';
+
+
+            });
+
+        }
+        if (this.stationObject.extMetadata !== null) {
+            html += '<h2>Metadata URL:</h2> <a target="_blank"  href="' + this.stationObject.extMetadata + '">' + this.stationObject.extMetadata + '</a>' + '<br>';
+        }
+        if (this.stationObject.comment !== null) {
+            html += '<h2>Comment:</h2> ' + this.stationObject.comment + '<br>';
+        }
+
+        html += '<br>\
 			<table class="popup"><thead><tr><th> Data available </th><th> Variables measured </th></tr></thead>\
-			<tr><td>from: '+this.stationObject.minimum+'<br> to: '+this.stationObject.maximum+' </td><td>'+this.stationObject.variableText+'</td></tr>\
+			<tr><td>from: ' + this.stationObject.minimum + '<br> to: ' + this.stationObject.maximum + ' </td><td>' + this.stationObject.variableText + '</td></tr>\
 			<thead><tr><th> Soil Moisture measured at </th><th> Soil moisture measured with </th></tr></thead>\
-			<tr><td>'+this.stationObject.depthText+'</td><td>'+this.stationObject.sensorText+'</td></tr></table>';
-  // set htmlinfo without buttons to be reused later
-	this.htmlInfo = html;	
+			<tr><td>' + this.stationObject.depthText + '</td><td>' + this.stationObject.sensorText + '</td></tr></table>';
+        // set htmlinfo without buttons to be reused later
+        this.htmlInfo = html;
 
-    html += '<br><a href="#", id="viewData" class="Button">View Data</a>';
+        html += '<br><a href="#", id="viewData" class="Button">View Data</a>';
 
-			
-	  if(this.validationButton&&this.hasRecordsInDateRange(new Date(2007,01,01).getTime(),new Date(2012,11,31).getTime())){
-        html+='<a href="#" id="compareData" class="Button">Compare to CCI</a>';
-    }
-	
-	html+='</div>';
-	
-	if(this.stationObject.minimum=='never')
-	{
-		var html="<div class='iwindow'><br><h2>Station:</h2> "+this.name+" ("+this.stationObject.station_abbr+")<br><h2>Network:</h2> "+this.network.name + '<br>\
+
+        if (this.validationButton && this.hasRecordsInDateRange(new Date(2007, 01, 01).getTime(), new Date(2012, 11, 31).getTime())) {
+            html += '<a href="#" id="compareData" class="Button">Compare to CCI</a>';
+        }
+
+        html += '</div>';
+
+        if (this.stationObject.minimum == 'never') {
+            var html = "<div class='iwindow'><br><h2>Station:</h2> " + this.name + " (" + this.stationObject.station_abbr + ")<br><h2>Network:</h2> " + this.network.name + '<br>\
 		<br>No Data available</div>';
-	}
-	
-	this.marker.setInfoWindow(html,this);
+        }
 
-}
-/** checks if station has data in date range
-	@param {long} start - time in milliseconds from epoch
-	@param {long} end - time in milliseconds from epoch
-*/
-Station.prototype.hasRecordsInDateRange=function(start,end){
+        this.marker.setInfoWindow(html, this);
 
-	if(this.endDate.getTime()>=start&&this.startDate.getTime()<=end)return true;
-	return false;
+};
+    /** checks if station has data in date range
+    	@param {long} start - time in milliseconds from epoch
+    	@param {long} end - time in milliseconds from epoch
+    */
+Station.prototype.hasRecordsInDateRange = function(start, end) {
 
-}
+    if (this.endDate.getTime() >= start && this.startDate.getTime() <= end) return true;
+    return false;
+
+};
 
 /** opens the data viewer belonging to this station
-*/
-Station.prototype.openDataViewer=function(){
+ */
+Station.prototype.openDataViewer = function() {
 
-	this.dataviewer.openViewer(this);
-}
+    this.dataviewer.openViewer(this);
+};
 
 /** shows the station
-*/
-Station.prototype.show= function(){
+ */
+Station.prototype.show = function() {
 
-	this.marker.show();
-	this.visible=true;
+        this.marker.show();
+        this.visible = true;
 
-}
-/** hides the station
-*/
-Station.prototype.hide= function(){
+};
+    /** hides the station
+     */
+Station.prototype.hide = function() {
 
-	this.marker.hide();
-	this.visible=false;
+    this.marker.hide();
+    this.visible = false;
 
-}
+};
 
 /** set station marker with respect to zoom level
 	currently not used
 	@param {int} zoom - current zoom level
 */
-Station.prototype.setMarkerIconsWithRespectToZoom=function(zoom){
+Station.prototype.setMarkerIconsWithRespectToZoom = function(zoom) {
 
-	this.marker.setIconWithRespectToZoom(zoom);
-}
+    this.marker.setIconWithRespectToZoom(zoom);
+};
 
 /** get location of station
 	@returns {object} google.maps.LatLng object
 */
-Station.prototype.getLocation= function(){
+Station.prototype.getLocation = function() {
 
-	return this.location;
-}
-/** get latitude of station
-	@returns {float} latitude
-*/
-Station.prototype.getLatitude= function(){
+        return this.location;
+};
+    /** get latitude of station
+    	@returns {float} latitude
+    */
+Station.prototype.getLatitude = function() {
 
-	return this.location.lat();
-}
-/** get longitude of station
-	@returns {float} longitude
-*/
-Station.prototype.getLongitude= function(){
+        return this.location.lat();
+};
+    /** get longitude of station
+    	@returns {float} longitude
+    */
+Station.prototype.getLongitude = function() {
 
-	return this.location.lng();
-}
+    return this.location.lng();
+};
 
-Station.prototype.activateValidationFunction=function(){
+Station.prototype.activateValidationFunction = function() {
 
-	this.validationButton=true;
-	this.validationViewer=new ValidationViewer(this.id+'_dv');
-	this.generateInfoWindow();
-	
-}
+    this.validationButton = true;
+    this.validationViewer = new ValidationViewer(this.id + '_dv');
+    this.generateInfoWindow();
 
-Station.prototype.openDataComparison=function(){
+};
 
-	this.validationViewer.openViewer(this);
+Station.prototype.openDataComparison = function() {
 
-}
+    this.validationViewer.openViewer(this);
 
-Station.prototype.focusMapOnMe=function(){
+};
 
-	this.map.setCenter(this.location);
-	this.map.setZoom(7);
+Station.prototype.focusMapOnMe = function() {
 
-}
+    this.map.setCenter(this.location);
+    this.map.setZoom(7);
+
+};
